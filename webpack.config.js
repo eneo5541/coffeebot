2904b5d35498b6abf5846dd39bbb41a0875d586c
Eric Neo <eric.neo@qantashotels.com>
2020-11-06 13:47:49 +1100

    Move dotenv to dev dependency, as it is baked into lambda

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.js',
+  entry: ['./index.js'],
   target: 'node',
   output: {
+    path: `${process.cwd()}/dist`,
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    libraryTarget: 'umd'
+  }
 };
